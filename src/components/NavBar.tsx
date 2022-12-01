f39a305dd05c2dfa2bf015263e6501331cfbbe36
bpnguyen107 <bpnguyen107@g.ucla.edu>
2022-12-01 00:10:11 -0800

    added labels to navbar buttons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,16 +2,19 @@ import NavBarButton from "./NavBarButton";
 import { BarChart, BookOpen, FileText, Info, Settings } from "react-feather";
 
 const NavBar = () => {
+
+  const labels: string[] = []
+
   return (
     <div className="h-screen w-20 flex flex-col">
       <div className="h-fit flex flex-col">
-        <NavBarButton icon={<BarChart size="50" color="#0E6CB6" strokeWidth="1px" />} />
-        <NavBarButton icon={<BookOpen size="50" color="#0E6CB6" strokeWidth="1px" />} />
-        <NavBarButton icon={<FileText size="50" color="#0E6CB6" strokeWidth="1px" />} />
+        <NavBarButton icon={<BarChart size="50" color="#0E6CB6" strokeWidth="1px" />} label="Inputs" />
+        <NavBarButton icon={<BookOpen size="50" color="#0E6CB6" strokeWidth="1px" />} label="History" />
+        <NavBarButton icon={<FileText size="50" color="#0E6CB6" strokeWidth="1px" />} label="Data" />
       </div>
       <div className="grow flex flex-col-reverse">
-        <NavBarButton icon={<Info size="50" color="#0E6CB6" strokeWidth="1px" />} />
-        <NavBarButton icon={<Settings size="50" color="#0E6CB6" strokeWidth="1px" />} />
+        <NavBarButton icon={<Info size="50" color="#0E6CB6" strokeWidth="1px" />} label="Info" />
+        <NavBarButton icon={<Settings size="50" color="#0E6CB6" strokeWidth="1px" />} label="Settings" />
       </div>
     </div>
   );
--- src/components/NavBarButton.tsx
+++ src/components/NavBarButton.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-const NavBarButton = ({ icon }: { icon: React.ReactNode }) => {
+const NavBarButton = ({ icon, label }: { icon: React.ReactNode, label: string }) => {
   return (
     <div className="relative flex items-center justify-center w-20 h-20 hover:bg-epalightblue">
       {icon}
+      <div className="absolute bottom-0 left-50 text-epablue text-xs">
+        {label}
+      </div>
     </div>
   );
 };
