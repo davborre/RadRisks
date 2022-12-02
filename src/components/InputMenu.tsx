2219e651259a4677901b537569b29a1eb659eece
bpnguyen107 <bpnguyen107@g.ucla.edu>
2022-12-02 00:20:04 -0800

    added other input dropdowns

--- src/components/InputMenu.tsx
+++ src/components/InputMenu.tsx
@@ -1,14 +1,34 @@
 import Dropdown from "./Dropdown";
 import { radionuclides } from "../data/radionuclides";
+import { ages } from "../data/ages";
 
 const InputMenu = () => {
 
+  const intakeMethods: string[] = ["Ingestion", "Inhalation"];
+  const sexes: string[] = ["Male", "Female"];
+
   return (
-    <div className="h-screen w-80 bg-epasagegreen pl-2 pt-4">
-      <h1 className="font-bold pb-10">Inputs</h1>
+    <div className="h-screen w-80 bg-epasagegreen pl-2 pt-4 flex flex-col gap-10">
+      <h1 className="font-bold">Inputs</h1>
       <div className="flex flex-row gap-5">
         <h2>Radionuclides:</h2>
         <Dropdown options={radionuclides} />
+      </div>
+      <div className="flex flex-row gap-5">
+        <h2>Intake method:</h2>
+        <Dropdown options={intakeMethods} />
+      </div>
+      <div className="flex flex-row gap-5">
+        <h2>Age at exposure:</h2>
+        <Dropdown options={ages} />
+      </div>
+      <div className="flex flex-row gap-5">
+        <h2>Length of exposure:</h2>
+        <Dropdown options={ages} />
+      </div>
+      <div className="flex flex-row gap-5">
+        <h2>Sex:</h2>
+        <Dropdown options={sexes} />
       </div>
     </div>
   );
