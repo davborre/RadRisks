76772f8ea5238ce161b8370d1e29ab13f26febc9
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-02-02 12:15:02 -0800

    usage function data loaded from backend

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import { invoke } from '@tauri-apps/api/tauri'
+import { useEffect, useState } from 'react'
 
 const DataTable = () => {
+  const [usage, setUsage] = useState({});
+
+  useEffect(() => {
+    invoke("usage")
+      .then((res: any) => {
+        setUsage(res)
+      })
+  });
+
   return (
     <table className="table-auto mx-auto text-left mt-5">
       <thead className="bg-epablue text-white">
         <tr>
           <th />
-          <th colSpan={2}>Inhalation (m3/d)</th>
+          <th colSpan={2}>Inhalation</th>
           <th colSpan={2}>Tapwater Usage (L/d)</th>
           <th colSpan={2}>Dietary (kcal/d)</th>
         </tr>
@@ -21,33 +31,19 @@ const DataTable = () => {
         </tr>
       </thead>
       <tbody>
-        <tr className="bg-epalightblue">
-          <td  >0</td>
-          <td  >2.900E+00</td>
-          <td  >2.900E+00</td>
-          <td  >1.911E-01</td>
-          <td  >1.880E-01</td>
-          <td  >4.777E+02</td>
-          <td  >4.700E+02</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>5.200E+00</td>
-          <td>5.200E+00</td>
-          <td>2.225E-01</td>
-          <td>2.162E-01</td>
-          <td>7.912E+02</td>
-          <td>7.517E+02</td>
-        </tr>
-        <tr className="bg-epalightblue">
-          <td>2</td>
-          <td>6.480E+00</td>
-          <td>6.480E+00</td>
-          <td>2.856E-01</td>
-          <td>2.725E-01</td>
-          <td>1.109E+03</td>
-          <td>1.033E+03</td>
-        </tr>
+        {Object.entries(usage).map((entries: any) => {
+          return (
+            <tr className="odd:bg-epalightblue">
+              <td> {entries[0]} </td>
+              <td> {entries[1][0].toExponential(3)} </td>
+              <td> {entries[1][1].toExponential(3)} </td>
+              <td> {entries[1][2].toExponential(3)} </td>
+              <td> {entries[1][3].toExponential(3)} </td>
+              <td> {entries[1][4].toExponential(3)} </td>
+              <td> {entries[1][5].toExponential(3)} </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   )
