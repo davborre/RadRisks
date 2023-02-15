e462cb91b182d17598f0b84659cc913522df36a0
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-02-14 22:54:51 -0800

    populated data menu with radionuclides

--- src/components/DataMenu.tsx
+++ src/components/DataMenu.tsx
@@ -1,50 +1,81 @@
 import { ChevronDown } from "react-feather";
+import { radionuclides } from "../data/radionuclides";
 
 const DataMenu = ({ setTable }: { setTable: React.Dispatch<React.SetStateAction<number>> }) => {
   return (
     <div className="h-screen w-80 bg-epasagegreen pt-4 flex flex-col overflow-auto">
       <h1 className="font-bold pl-2 mb-8">Data</h1>
-      <details className="even:bg-epaolivegreen p-2 select-none group">
+      <details className="even:bg-epaolivegreen p-2 select-none group max-h-96 overflow-auto">
         <summary className="flex">
-          Risk Coefficients
+          Ingestion Risk Coefficients
           <ChevronDown className="ml-auto group-open:rotate-180" />
         </summary>
-        <ul className="pl-2 pt-2 space-y-2 list-none text-sm">
-          <li>Ac-224</li>
-          <ul className="pl-4">
-            <li>Esophagus</li>
-            <li>Stomach</li>
-            <li>Colon</li>
-            <li>...</li>
-          </ul>
-          <li>Ac-225</li>
-          <li>Ac-226</li>
-          <li>...</li>
-        </ul>
+        {radionuclides.map((radionuclide: string) => {
+          return (
+            <details className="pl-2 pt-2 space-y-2 text-sm">
+              <summary>
+                {radionuclide}
+              </summary>
+              <ul className="pl-4">
+                <li>Esophagus</li>
+                <li>Stomach</li>
+                <li>Colon</li>
+                <li>Liver</li>
+                <li>Lung</li>
+                <li>Bone</li>
+                <li>Skin</li>
+                <li>Breast</li>
+                <li>Ovary</li>
+                <li>Bladder</li>
+                <li>Kidney</li>
+                <li>Thyroid</li>
+                <li>Leukemia</li>
+                <li>Residual</li>
+              </ul>
+            </details>
+          );
+        })}
       </details>
-      <details className="even:bg-epaolivegreen p-2 select-none group">
+      <details className="even:bg-epaolivegreen p-2 select-none group max-h-96 overflow-auto">
+        <summary className="flex">
+          Inhalation Risk Coefficients
+          <ChevronDown className="ml-auto group-open:rotate-180" />
+        </summary>
+        {radionuclides.map((radionuclide: string) => {
+          return (
+            <details className="pl-2 pt-2 space-y-2 text-sm">
+              <summary>
+                {radionuclide}
+              </summary>
+              <ul className="pl-4">
+                <li>Esophagus</li>
+                <li>Stomach</li>
+                <li>Colon</li>
+                <li>Liver</li>
+                <li>Lung</li>
+                <li>Bone</li>
+                <li>Skin</li>
+                <li>Breast</li>
+                <li>Ovary</li>
+                <li>Bladder</li>
+                <li>Kidney</li>
+                <li>Thyroid</li>
+                <li>Leukemia</li>
+                <li>Residual</li>
+              </ul>
+            </details>
+          );
+        })}
+      </details>
+      <details className="even:bg-epaolivegreen p-2">
         <summary className="flex" onClick={() => setTable(0)}>
           Usage Function
-          <ChevronDown className="ml-auto group-open:rotate-180" />
         </summary>
-        <ul className="pl-2 pt-2 space-y-2 list-none text-sm">
-          <li>Inhalation</li>
-          <li>Tapwater Usage</li>
-          <li>Dietary Ingestion</li>
-          <li>All</li>
-        </ul>
       </details>
-      <details className="even:bg-epaolivegreen p-2 select-none group">
+      <details className="even:bg-epaolivegreen p-2">
         <summary className="flex" onClick={() => setTable(1)}>
           Survival Function
-          <ChevronDown className="ml-auto group-open:rotate-180" />
         </summary>
-        <ul className="pl-2 pt-2 space-y-2 list-none text-sm">
-          <li>Extrapolated Survival</li>
-          <li>Average Remaining Lifetime</li>
-          <li>Male to Female Ratio</li>
-          <li>All</li>
-        </ul>
       </details>
     </div>
   );
