e24baeae09ba280bb9cca5a81368805a8445c745
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-02-16 01:42:48 -0800

    fixed script

--- scripts/INHINGtoRS.py
+++ scripts/INHINGtoRS.py
@@ -8,7 +8,8 @@ for file in files:
   with file.open('r') as file_handle:
     fileName = file.name
     period = fileName.find(".")
-    radionuclide = fileName[:(period - 1)]
+    radionuclideWithDash = fileName[:(period - 1)]
+    radionuclide = radionuclideWithDash[:radionuclideWithDash.find("-")] + radionuclideWithDash[radionuclideWithDash.find("-") + 1:]
     absorptionType = fileName[(period - 1):period]
 
     outputFileName = "src-tauri/src/data/" + radionuclide + ".rs"
@@ -19,10 +20,11 @@ for file in files:
         cancer = line.split()[2]
         with open(outputFileName, "a") as output_file:
           output_file.write("#[tauri::command]\n")
-          output_file.write("pub fn " + radionuclide + absorptionType + cancer + "() -> HashMap<u8, [f32;7]> {\n")
+          output_file.write("pub fn " + radionuclide + absorptionType + cancer + "() -> HashMap<u8, [f32;6]> {\n")
           output_file.write("  let data = HashMap::from([\n")
       elif (line.split()[0] == "110"):
         with open(outputFileName, "a") as output_file:
+          output_file.write("    (" + splitLine[0] + ", [" + splitLine[1] + ", " + splitLine[2] + ", " + splitLine[3] + ", " + splitLine[4] + ", " + splitLine[5] + ", " + splitLine[6] + "]),\n")
           output_file.write("  ]);\n")
           output_file.write("  return data\n")
           output_file.write("}\n")
