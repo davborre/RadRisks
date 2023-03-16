2dbb5fccfb7a5ec605479dcb8943958bbbee7a75
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-03-16 01:40:51 -0700

    tables for each absorption type display

--- src/components/CalculationsTable.tsx
+++ src/components/CalculationsTable.tsx
@@ -5,6 +5,7 @@ import { cancers } from '../data/cancers';
 
 const CalculationsTable = ({ calculation }: { calculation: any }) => {
   const [tables, setTables] = useState([]);
+  const [absorptionTypes, setAbsorptionTypes] = useState<string[]>([]);
 
   const { radionuclide, age, exposureLength, intakeMethod } = calculation;
 
@@ -13,43 +14,49 @@ const CalculationsTable = ({ calculation }: { calculation: any }) => {
       const newTables: any = [];
       const usageTable: any = await invoke('usage');
       const survivalTable: any = await invoke('survival');
-      const table: any = {}
+      const types: string = await invoke('inhalation_types', { radionuclide: radionuclide })
+      const absorptionTypes = types.split("-");
+      setAbsorptionTypes(absorptionTypes);
+      console.log(absorptionTypes);
 
-      for (let i = 0; i < cancers.length; i++) {
-        const calculations: number[] = [];
-        const riskCoefficientsTable: any = await invoke(`plugin:${intakeMethod}_${radionuclide}|${cancers[i]}_f`);
-        for (let j = 0; j < 6; j++) {
-          let lifetimeRisk = 0;
-          let unitIntake = 0;
-          const startingYear = age;
-          const endingYear = age + exposureLength;
-          let survCol;
-          let usageCol;
-          if (j == 0 || j == 3) {
-            survCol = 1;
-            usageCol = 0;
-          } else if (j == 1 || j == 4) {
-            survCol = 2;
-            usageCol = 1;
-          } else {
-            survCol = 0;
-            usageCol = 0;
-          }
-          for (let k = startingYear; k <= endingYear; k++) {
-            if (k == startingYear || k == endingYear) {
-              lifetimeRisk += 0.5 * survivalTable[k][survCol] * usageTable[k][usageCol] * 365 * riskCoefficientsTable[k][j];
-              unitIntake += 0.5 * survivalTable[k][survCol] * usageTable[k][usageCol] * 365;
+      for (let absorptionType = 0; absorptionType < absorptionTypes.length; absorptionType++) {
+        const table: any = {}
+        for (let i = 0; i < cancers.length; i++) {
+          const calculations: number[] = [];
+          const riskCoefficientsTable: any = await invoke(`plugin:${intakeMethod}_${radionuclide}|${cancers[i]}_${absorptionTypes[absorptionType]}`);
+          for (let j = 0; j < 6; j++) {
+            let lifetimeRisk = 0;
+            let unitIntake = 0;
+            const startingYear = age;
+            const endingYear = age + exposureLength;
+            let survCol;
+            let usageCol;
+            if (j == 0 || j == 3) {
+              survCol = 1;
+              usageCol = 0;
+            } else if (j == 1 || j == 4) {
+              survCol = 2;
+              usageCol = 1;
+            } else {
+              survCol = 0;
+              usageCol = 0;
             }
-            else {
-              lifetimeRisk += survivalTable[k][survCol] * usageTable[k][usageCol] * 365 * riskCoefficientsTable[k][j];
-              unitIntake += survivalTable[k][survCol] * usageTable[k][usageCol] * 365;
+            for (let k = startingYear; k <= endingYear; k++) {
+              if (k == startingYear || k == endingYear) {
+                lifetimeRisk += 0.5 * survivalTable[k][survCol] * usageTable[k][usageCol] * 365 * riskCoefficientsTable[k][j];
+                unitIntake += 0.5 * survivalTable[k][survCol] * usageTable[k][usageCol] * 365;
+              }
+              else {
+                lifetimeRisk += survivalTable[k][survCol] * usageTable[k][usageCol] * 365 * riskCoefficientsTable[k][j];
+                unitIntake += survivalTable[k][survCol] * usageTable[k][usageCol] * 365;
+              }
             }
+            calculations.push(lifetimeRisk / unitIntake);
           }
-          calculations.push(lifetimeRisk / unitIntake);
+          table[cancers[i]] = calculations;
         }
-        table[cancers[i]] = calculations;
+        newTables.push(table);
       }
-      newTables.push(table);
       setTables(newTables);
     })();
   }, [calculation])
@@ -58,39 +65,42 @@ const CalculationsTable = ({ calculation }: { calculation: any }) => {
     <div>
       {tables.map((table, i) => {
         return (
-          <table key={i} className="table-auto mx-auto text-left mt-5">
-            <thead className="bg-epablue text-white">
-              <tr>
-                <th />
-                <th colSpan={3}>Mortality (/Bq)</th>
-                <th colSpan={3}>Morbidity (/Bq)</th>
-              </tr>
-              <tr>
-                <th className="pr-10">Age</th>
-                <th className="pr-20">Male</th>
-                <th className="pr-20">Female</th>
-                <th className="pr-20">Both</th>
-                <th className="pr-20">Male</th>
-                <th className="pr-20">Female</th>
-                <th className="pr-20">Both</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(table).map((entries: any, j) => {
-                return (
-                  <tr key={j} className="odd:bg-epalightblue">
-                    <td> {entries[0]} </td>
-                    <td> {entries[1][0].toExponential(2)} </td>
-                    <td> {entries[1][1].toExponential(2)} </td>
-                    <td> {entries[1][2].toExponential(2)} </td>
-                    <td> {entries[1][3].toExponential(2)} </td>
-                    <td> {entries[1][4].toExponential(2)} </td>
-                    <td> {entries[1][5].toExponential(2)} </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+          <div>
+            <h1 className="text-center font-bold text-2xl">{radionuclide} {age}-{age + exposureLength} {absorptionTypes[i]}</h1>
+            <table key={i} className="table-auto mx-auto text-left my-5">
+              <thead className="bg-epablue text-white">
+                <tr>
+                  <th />
+                  <th colSpan={3}>Mortality (/Bq)</th>
+                  <th colSpan={3}>Morbidity (/Bq)</th>
+                </tr>
+                <tr>
+                  <th className="pr-10">Age</th>
+                  <th className="pr-20">Male</th>
+                  <th className="pr-20">Female</th>
+                  <th className="pr-20">Both</th>
+                  <th className="pr-20">Male</th>
+                  <th className="pr-20">Female</th>
+                  <th className="pr-20">Both</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.entries(table).map((entries: any, j) => {
+                  return (
+                    <tr key={j} className="odd:bg-epalightblue">
+                      <td> {entries[0]} </td>
+                      <td> {entries[1][0].toExponential(2)} </td>
+                      <td> {entries[1][1].toExponential(2)} </td>
+                      <td> {entries[1][2].toExponential(2)} </td>
+                      <td> {entries[1][3].toExponential(2)} </td>
+                      <td> {entries[1][4].toExponential(2)} </td>
+                      <td> {entries[1][5].toExponential(2)} </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         );
       })
       }
