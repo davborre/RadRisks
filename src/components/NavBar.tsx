aa5c7344c48da369f438910ec833f94749146b64
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-04-06 00:18:31 -0700

    modal works with new routing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,18 +2,23 @@ import NavBarButton from "./NavBarButton";
 import { BarChart, BookOpen, FileText, Info, Settings } from "react-feather";
 import React from "react";
 
-const NavBar = ({ setMenu }: { setMenu: React.Dispatch<React.SetStateAction<boolean[]>> }) => {
+const NavBar = ({ setOpen }: { setOpen: React.Dispatch<React.SetStateAction<boolean>> }) => {
 
   return (
     <div className="h-screen w-20 flex flex-col overflow-auto">
       <div className="h-fit flex flex-col">
-        <NavBarButton setMenu={setMenu} icon={<BarChart size="50" color="#0E6CB6" strokeWidth="1px" />} label="Inputs" />
-        <NavBarButton setMenu={setMenu} icon={<BookOpen size="50" color="#0E6CB6" strokeWidth="1px" />} label="History" />
-        <NavBarButton setMenu={setMenu} icon={<FileText size="50" color="#0E6CB6" strokeWidth="1px" />} label="Data" />
+        <NavBarButton icon={<BarChart size="50" color="#0E6CB6" strokeWidth="1px" />} label="Inputs" route="/" />
+        <NavBarButton icon={<BookOpen size="50" color="#0E6CB6" strokeWidth="1px" />} label="History" route="history" />
+        <NavBarButton icon={<FileText size="50" color="#0E6CB6" strokeWidth="1px" />} label="Data" route="data" />
       </div>
       <div className="grow flex flex-col-reverse">
-        <NavBarButton setMenu={setMenu} icon={<Info size="50" color="#0E6CB6" strokeWidth="1px" />} label="Info" />
-        <NavBarButton setMenu={setMenu} icon={<Settings size="50" color="#0E6CB6" strokeWidth="1px" />} label="Settings" />
+        <div onClick={() => setOpen(true)} className="relative flex items-center justify-center w-20 h-20 hover:bg-epalightblue">
+          <Info size="50" color="#0E6CB6" strokeWidth="1px" />
+          <div className="absolute bottom-0 left-50 text-epablue text-xs">
+            Info
+          </div>
+        </div>
+        <NavBarButton icon={<Settings size="50" color="#0E6CB6" strokeWidth="1px" />} label="Settings" route="settings" />
       </div>
     </div>
   );
--- src/components/NavBarButton.tsx
+++ src/components/NavBarButton.tsx
@@ -1,32 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-const NavBarButton = ({ icon, label, setMenu }: { icon: React.ReactNode, label: string, setMenu: React.Dispatch<React.SetStateAction<boolean[]>> }) => {
-
-  function handleMenuChange(e: React.MouseEvent) {
-    if (label == "Inputs") {
-      setMenu([true, false, false, false, false]);
-    }
-    else if (label == "History") {
-      setMenu([false, true, false, false, false]);
-    }
-    else if (label == "Data") {
-      setMenu([false, false, true, false, false]);
-    }
-    else if (label == "Settings") {
-      setMenu([false, false, false, true, false]);
-    }
-    else if (label == "Info") {
-      setMenu([true, false, false, false, true]);
-    }
-  }
+const NavBarButton = ({ icon, label, route }: { icon: React.ReactNode, label: string, route: string }) => {
 
   return (
-    <div onClick={handleMenuChange} className="relative flex items-center justify-center w-20 h-20 hover:bg-epalightblue">
-      {icon}
-      <div className="absolute bottom-0 left-50 text-epablue text-xs">
-        {label}
+    <NavLink to={route}
+      className={({ isActive, isPending }) => isActive ? 'active bg-epalightblue' : isPending ? 'pending bg-epalightblue' : ''}
+    >
+      <div className="relative flex items-center justify-center w-20 h-20 hover:bg-epalightblue">
+        {icon}
+        <div className="absolute bottom-0 left-50 text-epablue text-xs">
+          {label}
+        </div>
       </div>
-    </div>
+    </NavLink>
   );
 };
 
