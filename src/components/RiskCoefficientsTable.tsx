51973c5ffb66b888e69ad767b61aba1ab0a69ecf
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-04-07 00:37:49 -0700

    added active highlighting in data menu

--- src/components/RiskCoefficientsTable.tsx
+++ src/components/RiskCoefficientsTable.tsx
@@ -1,50 +1,68 @@
 import { invoke } from '@tauri-apps/api/tauri'
 import { useEffect, useState } from 'react'
 
-const RiskCoefficientsTable = ({ radionuclide, cancer }: { radionuclide: string, cancer: string }) => {
-  const [table, setTable] = useState({});
+const RiskCoefficientsTable = ({ radionuclide, cancer, intakeMethod }: { radionuclide: string, cancer: string, intakeMethod: string }) => {
+  const [tables, setTables] = useState([]);
 
   useEffect(() => {
-    invoke(`plugin:inh_${radionuclide}|${cancer}_s`)
-      .then((res: any) => {
-        setTable(res)
-      })
-  }, [radionuclide, cancer]);
+    (async () => {
+      const types: string = (intakeMethod == "inh") ? await invoke('inhalation_types', { radionuclide: radionuclide }) : await invoke('ingestion_types', { radionuclide: radionuclide });
+      const absorptionTypes = types.split("-");
+
+      const newTables: any = [];
+      for (let i = 0; i < absorptionTypes.length; i++) {
+        const newTable = await invoke(`plugin:${intakeMethod}_${radionuclide}|${cancer}_${absorptionTypes[i]}`);
+        newTables.push(newTable);
+      }
+
+      setTables(newTables);
+    })();
+  }, [radionuclide, cancer, intakeMethod]);
 
   return (
-    <table className="table-auto mx-auto text-left mt-5">
-      <thead className="bg-epablue text-white">
-        <tr>
-          <th />
-          <th colSpan={3}>Mortality (/Bq)</th>
-          <th colSpan={3}>Morbidity (/Bq)</th>
-        </tr>
-        <tr>
-          <th className="pr-10">Age</th>
-          <th className="pr-20">Male</th>
-          <th className="pr-20">Female</th>
-          <th className="pr-20">Both</th>
-          <th className="pr-20">Male</th>
-          <th className="pr-20">Female</th>
-          <th className="pr-20">Both</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Object.entries(table).map((entries: any) => {
+    <>
+      {
+        tables.map((table, i) => {
           return (
-            <tr className="odd:bg-epalightblue">
-              <td> {entries[0]} </td>
-              <td> {entries[1][0].toExponential(2)} </td>
-              <td> {entries[1][1].toExponential(2)} </td>
-              <td> {entries[1][2].toExponential(2)} </td>
-              <td> {entries[1][3].toExponential(2)} </td>
-              <td> {entries[1][4].toExponential(2)} </td>
-              <td> {entries[1][5].toExponential(2)} </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            <div>
+              <table className="table-auto mx-auto text-left mt-20">
+                <thead className="bg-epablue text-white">
+                  <tr>
+                    <th />
+                    <th colSpan={3}>Mortality (/Bq)</th>
+                    <th colSpan={3}>Morbidity (/Bq)</th>
+                  </tr>
+                  <tr>
+                    <th className="pr-10">Age</th>
+                    <th className="pr-20">Male</th>
+                    <th className="pr-20">Female</th>
+                    <th className="pr-20">Both</th>
+                    <th className="pr-20">Male</th>
+                    <th className="pr-20">Female</th>
+                    <th className="pr-20">Both</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.entries(table).map((entries: any) => {
+                    return (
+                      <tr className="odd:bg-epalightblue">
+                        <td> {entries[0]} </td>
+                        <td> {entries[1][0].toExponential(2)} </td>
+                        <td> {entries[1][1].toExponential(2)} </td>
+                        <td> {entries[1][2].toExponential(2)} </td>
+                        <td> {entries[1][3].toExponential(2)} </td>
+                        <td> {entries[1][4].toExponential(2)} </td>
+                        <td> {entries[1][5].toExponential(2)} </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          )
+        })
+      }
+    </>
   )
 }
 
--- src/routes/DataScreen.tsx
+++ src/routes/DataScreen.tsx
@@ -7,14 +7,15 @@ import SurvTable from "../components/SurvTable";
 const DataScreen = () => {
   const [radionuclide, setRadionuclide] = useState("");
   const [cancer, setCancer] = useState("");
+  const [intakeMethod, setIntakeMethod] = useState("");
   const [table, setTable] = useState(-1);
 
   return (
     <>
-      <DataMenu setRadionuclide={setRadionuclide} setCancer={setCancer} setTable={setTable} />
+      <DataMenu setRadionuclide={setRadionuclide} setCancer={setCancer} setTable={setTable} setIntakeMethod={setIntakeMethod} />
       <div className="grow p-20 h-screen overflow-auto">
         {table == 1 && <h1 className="text-center font-bold text-2xl"> {radionuclide} {cancer} </h1>}
-        {table == 1 && <RiskCoefficientsTable radionuclide={radionuclide} cancer={cancer} />}
+        {table == 1 && <RiskCoefficientsTable radionuclide={radionuclide} cancer={cancer} intakeMethod={intakeMethod} />}
         {table == 2 && <h1 className="text-center font-bold text-2xl"> Usage Function </h1>}
         {table == 2 && <UsageTable />}
         {table == 3 && <h1 className="text-center font-bold text-2xl"> Survival Function </h1>}
