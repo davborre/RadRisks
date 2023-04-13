128c92a36832a52f4986b7693aee850ee9990eb6
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-04-13 01:03:55 -0700

    fixed spacing in exported txt file

--- src/components/InputMenu.tsx
+++ src/components/InputMenu.tsx
@@ -16,9 +16,9 @@ async function formatTextContent(txtTables: Object[], lastCalculation: any): Pro
   const agePlusExposure = age.map((a: number, i: number) => a + exposureLengthYears[i]);
   const exposedString = `${radionuclide}, Exposed Ages: [${age.join(',')}]-[${agePlusExposure.join(',')}] and [${exposureLengthDays.join(',')}] Days`;
 
-  let text = ' '.repeat(26) + ((intakeMethod == 'inh') ? 'Inhalation' : 'Ingestion');
+  let text = ' '.repeat(24) + ((intakeMethod == 'inh') ? 'Inhalation' : 'Ingestion');
   text += ' Risk Coefficients\n'
-  text += ' '.repeat(22) + `${exposedString}\n`;
+  text += ' '.repeat((75 - exposedString.length) / 2) + `${exposedString}\n`;
 
   const types: string = (intakeMethod == "inh") ? await invoke('inhalation_types', { radionuclide: formattedRadionuclide }) : await invoke('ingestion_types', { radionuclide: formattedRadionuclide })
   const absorptionTypes = (intakeMethod == 'ing') ? types.split("-").concat(types.split("-")) : types.split("-");
@@ -27,7 +27,7 @@ async function formatTextContent(txtTables: Object[], lastCalculation: any): Pro
 
   for (let i = 0; i < txtTables.length; i++) {
     text += `
-                       Absorption Type: ${(intakeMethod == 'ing' && i < absorptionTypes.length / 2) ? 'Drinking Water' : (intakeMethod == 'ing') ? 'Diet' : ''}${(absorptionTypes[i] !== 'n' && intakeMethod == 'inh') ? absorptionTypes[i].toUpperCase() : (absorptionTypes[i] !== 'n' && intakeMethod == 'ing') ? ' (' + absorptionTypes[i].toUpperCase() + ')' : ''}
+                             Absorption Type: ${(intakeMethod == 'ing' && i < absorptionTypes.length / 2) ? 'Drinking Water' : (intakeMethod == 'ing') ? 'Diet' : ''}${(absorptionTypes[i] !== 'n' && intakeMethod == 'inh') ? absorptionTypes[i].toUpperCase() : (absorptionTypes[i] !== 'n' && intakeMethod == 'ing') ? ' (' + absorptionTypes[i].toUpperCase() + ')' : ''}
             --------Mortality (/Bq)--------   -------Morbidity (/Bq)------- 
  Cancer     Male        Female         Both   Male        Female        Both
  ---------------------------------------------------------------------------\n`;
