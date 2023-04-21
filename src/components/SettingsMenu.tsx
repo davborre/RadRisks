8ab409429a8c4b392e2e5d98327c7703a1aac2ad
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-04-21 00:52:23 -0700

    fixed settings menu header

--- src/components/SettingsMenu.tsx
+++ src/components/SettingsMenu.tsx
@@ -9,6 +9,7 @@ const SettingsMenu = () => {
   useEffect(() => {
     (async () => {
       const settings = new Store('.settings.dat');
+      console.log(await settings.entries());
       const fileType = await settings.get('fileType') as string;
       setFileType(fileType);
     })();
@@ -28,12 +29,15 @@ const SettingsMenu = () => {
 
   return (
     <div className="h-screen w-80 bg-epasagegreen px-2 pt-4 flex flex-col gap-10">
-      <h1 className="font-bold">Inputs</h1>
-      <div>
-        <label>Output File Directory:
-          <input className="w-full" type="text" />
-        </label>
-      </div>
+      <h1 className="font-bold">Settings</h1>
+      {/*
+        <div>
+          <label>Output File Directory:
+            <input className="w-full" type="text" />
+          </label>
+        </div>
+        */
+      }
       <div>
         <label>Output File Type:{' '}
           <Dropdown options={fileTypes} width={75} value={fileType} setValue={setFileType} />
