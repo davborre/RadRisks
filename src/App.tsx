f7db44f7f8b9556f1da26d417fd401cfcef78fc1
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-04-21 10:57:04 -0700

    darkmode changes at app level

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet, useOutletContext } from "react-router-dom";
+import { Store } from "tauri-plugin-store-api";
 import NavBar from "./components/NavBar";
 import Modal from "./components/Modal";
 
-
+type ContextType = [boolean, React.Dispatch<React.SetStateAction<boolean>>];
 
 function App() {
   const [open, setOpen] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const settings = new Store('.settings.dat');
+      const darkMode = await settings.get('darkMode') as boolean;
+      setDarkMode(darkMode);
+    })();
+  }, [])
 
   return (
-    <div className="flex">
+    <div className={`flex ${darkMode ? 'dark' : ''}`}>
       <NavBar setOpen={setOpen} />
-      <Outlet />
+      <Outlet context={[darkMode, setDarkMode]} />
       <Modal isOpen={open} setOpen={setOpen} />
     </div>
   );
+}
+
+export function useDarkMode() {
+  return useOutletContext<ContextType>();
 }
 
 export default App;
