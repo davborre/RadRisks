ce1c8de43a0416730c2ab5942ed64855625cd55c
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-04-21 01:18:54 -0700

    implemented working checkboxes in settings

--- src/components/SettingsMenu.tsx
+++ src/components/SettingsMenu.tsx
@@ -4,6 +4,8 @@ import { Store } from "tauri-plugin-store-api";
 
 const SettingsMenu = () => {
   const [fileType, setFileType] = useState<string | null>(null);
+  const [fractionalExposure, setFractionalExposure] = useState<boolean>(true);
+  const [darkMode, setDarkMode] = useState<boolean>(false);
   const fileTypes = ["pdf", "txt"];
 
   useEffect(() => {
@@ -11,9 +13,13 @@ const SettingsMenu = () => {
       const settings = new Store('.settings.dat');
       console.log(await settings.entries());
       const fileType = await settings.get('fileType') as string;
+      const fractionalExposure = await settings.get('fractionalExposure') as boolean;
+      const darkMode = await settings.get('darkMode') as boolean;
       setFileType(fileType);
+      setFractionalExposure(fractionalExposure);
+      setDarkMode(darkMode);
     })();
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (fileType == null) {
@@ -25,7 +31,20 @@ const SettingsMenu = () => {
       await settings.set('fileType', fileType);
       await settings.save();
     })();
-  }, [fileType])
+  }, [fileType]);
+
+  async function handleCheckbox(setting: string, checked: boolean) {
+    const settings = new Store('.settings.dat');
+
+    if (setting == 'fractionalExposure') {
+      setFractionalExposure(checked);
+    } else if (setting == 'darkMode') {
+      setDarkMode(checked);
+    }
+
+    await settings.set(setting, checked);
+    await settings.save();
+  }
 
   return (
     <div className="h-screen w-80 bg-epasagegreen px-2 pt-4 flex flex-col gap-10">
@@ -45,12 +64,12 @@ const SettingsMenu = () => {
       </div>
       <div>
         <label>Fractional Exposure:
-          <input className="scale-150 ml-2 accent-epablue" type="checkbox" />
+          <input className="scale-150 ml-2 accent-epablue" type="checkbox" checked={fractionalExposure} onChange={e => handleCheckbox('fractionalExposure', e.target.checked)} />
         </label>
       </div>
       <div>
         <label>Dark Mode:
-          <input className="scale-150 ml-2 accent-epablue" type="checkbox" />
+          <input className="scale-150 ml-2 accent-epablue" type="checkbox" checked={darkMode} onChange={e => handleCheckbox('darkMode', e.target.checked)} />
         </label>
       </div>
     </div>
