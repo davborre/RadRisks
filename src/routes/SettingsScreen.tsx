7e38fd0f006f809821fc06815b144e41f2a770eb
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-04-28 01:11:40 -0700

    added logo to settings screen

--- src/routes/SettingsScreen.tsx
+++ src/routes/SettingsScreen.tsx
@@ -4,6 +4,9 @@ const SettingsScreen = () => {
   return (
     <>
       <SettingsMenu />
+      <div className="grow h-screen flex justify-center items-center">
+        <img className="w-auto h-auto" src="public/logo.png" alt="RadRisks logo" />
+      </div>
     </>
   )
 }
