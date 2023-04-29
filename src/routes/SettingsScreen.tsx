8193b7d98e617d8a82b8de6d447ffa73ca2d967a
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-04-28 21:35:17 -0700

    extended dark mode to full settings screen

--- src/routes/SettingsScreen.tsx
+++ src/routes/SettingsScreen.tsx
@@ -4,7 +4,7 @@ const SettingsScreen = () => {
   return (
     <>
       <SettingsMenu />
-      <div className="grow h-screen flex justify-center items-center">
+      <div className="grow h-screen flex justify-center items-center dark:bg-neutral-900">
         <img className="w-auto h-auto" src="/logo.png" alt="RadRisks logo" />
       </div>
     </>
