1a7ed4f882fa1a938b2c07f867dc7e6bfb925286
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-05-21 00:21:40 -0700

    moved formatTextContent to utils

--- src/components/HistoryMenu.tsx
+++ src/components/HistoryMenu.tsx
@@ -1,41 +1,11 @@
 import { ChevronDown } from "react-feather";
 import { save } from "@tauri-apps/api/dialog";
 import { writeBinaryFile, writeTextFile } from "@tauri-apps/api/fs";
-import { invoke } from '@tauri-apps/api/tauri';
 import { useEffect, useState } from "react";
 import { Store } from "tauri-plugin-store-api";
 import MenuDescription from "./MenuDescription";
+import { formatTextContent } from "../utils";
 import jsPDF from "jspdf";
-
-async function formatTextContent(txtTables: Object[], lastCalculation: any): Promise<string> {
-  const { radionuclide, formattedRadionuclide, age, exposureLengthYears, exposureLengthDays, intakeMethod } = lastCalculation;
-
-  const agePlusExposure = age.map((a: number, i: number) => a + exposureLengthYears[i]);
-  const exposedString = `${radionuclide}, Exposed Ages: ${age.map((a: number, i: number) => `${age[i]}-${agePlusExposure[i]} Years and ${exposureLengthDays[i]} Days`).join(', ')}`;
-
-  let text = ' '.repeat(24) + ((intakeMethod == 'inh') ? 'Inhalation' : 'Ingestion');
-  text += ' Risk Coefficients\n'
-  text += ' '.repeat((75 - exposedString.length) / 2) + `${exposedString}\n`;
-
-  const types: string = (intakeMethod == "inh") ? await invoke('inhalation_types', { radionuclide: formattedRadionuclide }) : await invoke('ingestion_types', { radionuclide: formattedRadionuclide })
-  const absorptionTypes = (intakeMethod == 'ing') ? types.split("-").concat(types.split("-")) : types.split("-");
-
-  console.log(absorptionTypes)
-
-  for (let i = 0; i < txtTables.length; i++) {
-    text += `
-                             Absorption Type: ${(intakeMethod == 'ing' && i < absorptionTypes.length / 2) ? 'Drinking Water' : (intakeMethod == 'ing') ? 'Diet' : ''}${(absorptionTypes[i] !== 'n' && intakeMethod == 'inh') ? absorptionTypes[i].toUpperCase() : (absorptionTypes[i] !== 'n' && intakeMethod == 'ing') ? ' (' + absorptionTypes[i].toUpperCase() + ')' : ''}
-            --------Mortality (/Bq)--------   -------Morbidity (/Bq)------- 
- Cancer     Male        Female         Both   Male        Female        Both
- ---------------------------------------------------------------------------\n`;
-
-    Object.entries(txtTables[i]).map((entries) => {
-      text += ` ${entries[0]}${' '.repeat(12 - entries[0].length)}${entries[1].map((entry: number) => String(entry.toExponential(2)) + ' '.repeat(8 - String(entry.toExponential(2)).length)).join('   ')}\n`
-    })
-  }
-
-  return text;
-}
 
 const historyDescription = 'Select a radionuclide to expand a list of previous calculations. Select a calculation to display its data.'
 
